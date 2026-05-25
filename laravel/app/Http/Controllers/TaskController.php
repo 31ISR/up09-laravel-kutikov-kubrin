c722429f1d26a2b921c5a21dd3f0684fe687fb42
Bogdan <diveffff@gmail.com>
2026-05-25 04:11:43 +0000

    lar

--- laravel/app/Http/Controllers/TaskController.php
+++ laravel/app/Http/Controllers/TaskController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
     public function index(){
+        /** @var User $user */
+        $user = Auth::user();
+        $tasks = $user->tasks()->with('category')->latest()->paginate(10);
         
-    }
+        return view('tasks.index', compact('tasks'));
+        }
 
     public function store(){
         
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -6,24 +6,24 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\TaskController;
+use App\Models\Task;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // })->name('dashboard');
 
+Route::middleware('guest')-> group(function (){
 Route::get('/register', [RegisterController::class, 'show'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/login', [LoginController::class, 'show'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
+});
 
+Route::middleware('auth')->group(function(){
 Route::post('/logout', LogoutController::class)->name('logout');
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-Route::resource(
-    'categories',
-    CategoryController::class
-);
-Route::resource(
-    'tasks',
-    CategoryController::class
-);
+Route::resource('categories',CategoryController::class);
+Route::resource('tasks',TaskController::class);
+});
